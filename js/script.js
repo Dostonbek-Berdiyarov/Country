1cfa8b9346d65231950f4488294a658df46b0348
Dostonbek-Berdiyarov <dostonbey.0726@gmail.com>
2024-03-31 10:43:08 +0500

    Add form

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 const elThemeBtn = document.querySelector(".js-local-theme");
+const elSearchInput = document.querySelector(".js-input");
+const elCards = document.querySelectorAll(".js-cards");
+const elCardTitle = document.querySelectorAll(".js-card-title");
 
 elThemeBtn.addEventListener("click", () => {
   document.body.classList.toggle("theme");
 });
+
+// // elSearchInput.addEventListener("change", () => {
+// elSearchInput.addEventListener("input", (event) => {
+//   const searchText = event.target.value.toLowerCase();
+//   let found = false;
+//   elCardTitle.forEach((title) => {
+//     const titleText = title.textContent.toLowerCase();
+//     if (titleText.includes(searchText)) {
+//       found = true;
+//     }
+//   });
+//   if (found) {
+//     elCards.classList.add("block");
+//   } else {
+//     elCards.classList.remove("block");
+//   }
+// });
